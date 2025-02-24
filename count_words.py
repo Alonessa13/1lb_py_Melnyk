09b15bf9c5fc4a08eb2b038edfecc9a5a87eedb2
Daria <dasha130407daria@gmail.com>
2025-02-24 15:56:59 +0200

    Add files via upload

---
+++ count_words.py
@@ -0,0 +1,13 @@
+def count_words_in_file(filename):
+    try:
+        with open(filename, "r", encoding="utf-8") as file:
+            text = file.read()
+            words = text.split()
+            return len(words)
+    except FileNotFoundError:
+        print("Файл не знайдено.")
+        return 0
+
+
+word_count = count_words_in_file("quote.txt")
+print(f"Кількість слів у файлі: {word_count}")
